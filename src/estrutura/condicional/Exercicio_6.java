65e1989cdb62a373eacbaaa4ff2c9fc3f9a3449a
Leticia Takenaka <leticiatakenaka12@gmail.com>
2023-04-05 20:18:58 -0300

    Algorítmo retornando valor de função

---
+++ src/estrutura/condicional/Exercicio_6.java
@@ -0,0 +1,23 @@
+package estrutura.condicional;
+
+import java.util.Scanner;
+
+public class Exercicio_6 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    double x, f = 0;
+
+    System.out.print("Digite o valor de x: ");
+    x = input.nextDouble();
+
+    if (x < -2) {
+      f = 2 * x + 2;
+    } else if (-2 <= x && x < 3) {
+      f = 3;
+    } else {
+      f = -3;
+    }
+
+    System.out.format("O resultado da função é %.2f", f);
+  }
+}
